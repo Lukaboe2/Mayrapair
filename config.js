8c127aef63656bb75232e97ee4d3cfa7173bc103
Professor 🥼 <guruhtechlab@gmail.com>
2026-06-13 06:45:54 +0300

    Update configuration values for session and bot settings

--- config.js
+++ config.js
@@ -8,10 +8,12 @@ require('dotenv').config({
 
 module.exports = {
     PORT: process.env.PORT || 50900,
-    SESSION_PREFIX: process.env.SESSION_PREFIX || "Panther~",
-    GC_JID: process.env.GC_JID || "",
+    SESSION_PREFIX: process.env.SESSION_PREFIX || "GURU~",
+    GC_JID: process.env.GC_JID || "120363423900965397@g.us",
     DATABASE_URL: process.env.DATABASE_URL || "",
-    BOT_REPO: process.env.BOT_REPO || "https://github.com/GuruhTech/PANTHERR-X-ULTRA-PAIR",
-    WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029VbCpYtZLtOj5LDuj7Q1p",
-    MSG_FOOTER: process.env.MSG_FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴘᴀɴᴛʜᴇʀʀ x ᴜʟᴛʀᴀ*",
+    BOT_REPO: process.env.BOT_REPO || "https://github.com/GuruhTech/ULTRA-GURU",
+    WA_CHANNEL: process.env.WA_CHANNEL || "https://whatsapp.com/channel/0029Vb7jauLHLHQbkcbcHi0e",
+    MSG_FOOTER: process.env.MSG_FOOTER || "> *ᴘᴏᴡᴇʀᴇᴅ ʙʏ ᴜʟᴛʀᴀ ɢᴜʀᴜ ᴍᴅ*",
+    GC_INVITE: process.env.GC_INVITE || "https://chat.whatsapp.com/Cp6waPAdT3hLVcbdfBeV61?s=cl&p=a&mlu=4&amv=0",
+    CHANNEL_JID: process.env.CHANNEL_JID || "120363406649804510@newsletter",
 };
